050ed0c45f172e57f8544df5009d10baecb86fa7
Douglas Tott <douglasifbaianosb@gmail.com>
2021-12-11 14:57:01 +0000

    Criando class instutição

--- src/database/migrations/20211211145103_create_class_institutions.ts
+++ src/database/migrations/20211211145103_create_class_institutions.ts
@@ -1,14 +1,22 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('insitutions', (table) => {
+  return knex.schema.createTable('institutions', (table) => {
     table.increments('id').notNullable();
     table.string('name').notNullable();
     table.string('city').notNullable();
     table.string('country').notNullable();
+
+    table
+      .integer('member_id')
+      .notNullable()
+      .references('id')
+      .inTable('actors')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('insitutions');
+  return knex.schema.dropTable('institutions');
 }
