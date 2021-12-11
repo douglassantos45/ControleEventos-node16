05334da1d8c814bfb3aabcec0db330eeab210d34
Douglas Tott <douglasifbaianosb@gmail.com>
2021-12-11 14:52:38 +0000

    Criando classe dos atores do sistema

--- src/database/migrations/20211211142525_create_class_actors.ts
+++ src/database/migrations/20211211142525_create_class_actors.ts
@@ -1,7 +1,7 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('members', (table) => {
+  return knex.schema.createTable('actors', (table) => {
     table.increments('id').notNullable();
     table.string('type').notNullable();
 
@@ -16,5 +16,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('members');
+  return knex.schema.dropTable('actors');
 }
