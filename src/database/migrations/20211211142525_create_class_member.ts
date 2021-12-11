d008391ee5348b712802ca8b7ea2804da9f467e8
Douglas Tott <douglasifbaianosb@gmail.com>
2021-12-11 14:39:26 +0000

    Criando classe members

--- src/database/migrations/20211211142525_create_class_member.ts
+++ src/database/migrations/20211211142525_create_class_member.ts
@@ -3,6 +3,7 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('members', (table) => {
     table.increments('id').notNullable();
+    table.string('type').notNullable();
 
     table
       .integer('user_id')
