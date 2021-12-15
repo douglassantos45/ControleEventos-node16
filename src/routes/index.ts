d8ef3a6d3b18444402b6012d99bfcc8c98459e6d
Douglas Tott <douglasifbaianosb@gmail.com>
2021-12-15 12:50:19 +0000

    Configurando rota dos eventos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import UserController from '../controllers/users.controllers';
+import EventsController from '../controllers/events.controllers';
 
 const routes = Router();
 
 const userController = new UserController();
+const eventController = new EventsController();
 
 /* USERS */
 routes.get('/users', userController.index);
@@ -11,5 +13,10 @@ routes.post('/users', userController.store);
 routes.patch('/users/:id', userController.update);
 routes.delete('/users/:id', userController.remove);
 /* END USERS */
+
+/* EVENTS */
+routes.get('/events', eventController.index);
+routes.post('/events', eventController.store);
+/* END EVENTS */
 
 export default routes;
