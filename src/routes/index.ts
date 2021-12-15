ab0718a6333915384226ad495b4cf3193f9f7406
Douglas Tott <douglasifbaianosb@gmail.com>
2021-12-15 14:50:19 +0000

    Configurando rota do comitê

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import UserController from '../controllers/users.controllers';
 import EventsController from '../controllers/events.controllers';
 import ArticlesController from '../controllers/articles.controllers';
+import CommittieesControllers from '../controllers/committiees.controllers';
 
 const routes = Router();
 
 const userController = new UserController();
 const eventController = new EventsController();
 const articleController = new ArticlesController();
+const committieeController = new CommittieesControllers();
 
 /* USERS */
 routes.get('/users', userController.index);
@@ -25,5 +27,10 @@ routes.post('/events', eventController.store);
 routes.get('/articles', articleController.index);
 routes.post('/articles', articleController.store);
 /* END ARTICLES */
+
+/* COMMITTIEE */
+routes.get('/committiees', committieeController.store);
+routes.post('/committiees', committieeController.store);
+/* END COMMITTIEE */
 
 export default routes;
